37a96a6f7748da9baff418b6bab7da718ce90187
Sam Martin <me@smartin.io>
2022-10-29 22:27:52 +0200

    3 -> 2 trades

--- server/app/src/main/java/io/smartin/id1212/config/Rules.java
+++ server/app/src/main/java/io/smartin/id1212/config/Rules.java
@@ -6,15 +6,13 @@ import java.util.Map;
 import io.smartin.id1212.model.components.Hand.HandType;
 
 public class Rules {
-    public static final int    MAX_TRADES_PER_ROUND = 3;
+    public static final int    MAX_TRADES_PER_ROUND = 2;
     public static final int    ROUND_WIN_SCORE = 2;
     public static final int    WIN_WITH_TWO_SCORE = 5;
     public static final int    TRADE_BAN_SCORE = 45;
     public static final int    MAX_CARDS_PER_PLAYER = 5;
     public static final int    POINTS_FOR_WON_CHICAGO = 15;
     public static final int    MAX_GAME_SCORE = 52;
-    public static final int    LINGER_TIME_MS = 2500;
-    public static final int    GUARANTEED_WIN_LINGER_TIME_MS = 5000;
     public static final boolean CHICAGO_MUST_HAVE_BEST_HAND = false;
     public static final Map<HandType, Integer> HAND_SCORES = new HashMap<HandType, Integer>() {{
         put(HandType.NOTHING, 0);
